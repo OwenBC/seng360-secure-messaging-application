afc792b366ec89c939f8bec875822924c169802f
Alex <zthh626@gmail.com>
2021-11-08 23:10:53 -0800

    Remove files/Rename to SENG 350 Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex } from "@chakra-ui/layout";
+import Container from "./common/messages/containers/Container";
+import SidebarContainer from "./common/sidebar/containers/SidebarContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex bg="gray" flexDirection="row" height="100vh">
+      <SidebarContainer />
+      <Container />
+    </Flex>
   );
 }
 
