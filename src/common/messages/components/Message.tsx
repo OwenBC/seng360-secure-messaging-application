cbd6a1bc92eef51dd345ce64982ed776df18207f
Owen <ocrewe04@gmail.com>
2021-11-14 17:50:41 -0800

    Add messages components

---
+++ src/common/messages/components/Message.tsx
@@ -0,0 +1,46 @@
+import { Box, Flex, Text } from '@chakra-ui/layout';
+import { Heading } from '@chakra-ui/react';
+import React from 'react'
+
+const user = "user0";
+
+interface MessageProps {
+    name: string;
+    time: string;
+    text: string;
+    image: File | null;
+    onClick: () => void;
+}
+
+function Message({ name, time, text, image, onClick }: MessageProps) {
+    return (
+        <Flex 
+            flexDirection={name ===  user ? "row-reverse":"row"} 
+            padding="1em"
+        >
+            <Box maxW="sm" border="1px" paddingX="1em" borderColor="black" borderStyle="solid" borderRadius="15px" overflow="hidden">
+                <Heading fontSize="small">
+                    {name + ' - ' + time}
+                </Heading> 
+                <Text>
+                    {text}
+                </Text>
+            </Box>
+            <Heading
+                onClick={onClick}
+                textAlign="center"
+                _hover={{
+                  color: "gray",
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                }}
+                padding="1em"
+                margin="0em"
+            >
+                ...
+            </Heading>
+        </Flex>
+    );
+}
+
+export default Message
--- src/common/messages/containers/MessagesContainer.tsx
+++ src/common/messages/containers/MessagesContainer.tsx
@@ -1,7 +1,35 @@
 import { Flex } from "@chakra-ui/layout";
+import Message from "../components/Message";
+import MessageHeaderContainer from "./MessageHeaderContainer";
 
 function MessagesContainer() {
-  return <Flex flexDirection="column" height="100%" width="100%" />;
+  const messages = [ 
+    {
+      name: "user1",
+      time: "8:10 AM",
+      text: "Hey, is this working?",
+      image: null,
+    },
+    {
+      name: "user0",
+      time: "8:11 AM",
+      text: "I think it is",
+      image: null,
+    },
+  ];
+
+  return (
+    <>
+      <MessageHeaderContainer />
+      <Flex flexDirection="column" height="100%" width="100%">
+        {messages.map((message) => {
+          return (
+            <Message name={message.name} time={message.time} text={message.text} image={message.image} onClick={() => {}} />
+          );
+        })}
+      </Flex>
+    </>
+  );
 }
 
 export default MessagesContainer;
