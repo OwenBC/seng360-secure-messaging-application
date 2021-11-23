df296a2ac5f7700fafbce3aba839c2c481b9892b
Owen <ocrewe04@gmail.com>
2021-11-22 23:27:16 -0800

    can send message to server

--- src/common/bottombar/containers/BottombarContainer.tsx
+++ src/common/bottombar/containers/BottombarContainer.tsx
@@ -3,8 +3,27 @@ import { Flex } from "@chakra-ui/layout";
 import { Input } from "@chakra-ui/react";
 import Line from "../../../shared/Line";
 import BottombarButton from "../components/BottombarButton";
+import { useContext, useState } from "react";
+import Context, { ContextType } from "../../../lib/Context";
 
-function BottombarContainer() {
+interface BottombarContainerProps {
+	loggedInAs: string;
+	activeChat: string;
+}
+
+function BottombarContainer( {loggedInAs, activeChat} : BottombarContainerProps ) {
+  const [message, setMessage] = useState("");
+  const { socket, serverKeys } = useContext(Context) as ContextType;
+
+  const sendMessage = (message: any) => {
+    if (serverKeys.publicKey === undefined) return;
+    socket.sendMessage(
+      serverKeys.publicKey?.encrypt(
+        `[s]:[${loggedInAs}_${activeChat}]:[message_id]:[${message}]:[${Date()}]`// Figure out what to put here
+      )
+    );
+  };
+
   return (
     <>
       <Line />
@@ -18,9 +37,18 @@ function BottombarContainer() {
       >
         <BottombarButton icon={<AddIcon />} onClick={() => {}} />
         <Flex marginY="0.75em" borderRadius="1em" grow={1}>
-          <Input padding="0.75em" width="100%" placeholder="Message" />
+          <Input padding="0.75em" width="100%" placeholder="Message" 
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+            onKeyUpCapture={(event) => {
+              if (event.code === "Enter") {
+                sendMessage(message);
+              }
+            }}
+          />
         </Flex>
-        <BottombarButton icon={<ChatIcon />} onClick={() => {}} />
+        <BottombarButton icon={<ChatIcon />} onClick={() => sendMessage(message)} />
       </Flex>
     </>
   );
